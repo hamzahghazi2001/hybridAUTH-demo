43e76cf967b395b3fe5fb8f780f0ea07e4d28694
Hamza Ghazi <77533830+hamzahghazi2001@users.noreply.github.com>
2026-01-16 16:26:36 +0400

    main page

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 @app.route('/health', methods=['GET'])
 def health():
     health_data = {"ok": True}
     return jsonify(health_data)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
